73e69df0e4214e2c03cd5292aea106639c696044
Kim Jae Hyeok <master_catch@naver.com>
2026-06-12 18:04:04 +0900

    fix : 메인페이지 수정 작업
    1. Header, Footer 컴포넌트 추가
    2. 타이틀 제목 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+'use client';
 import Link from 'next/link';
 import { scenarios  } from '@/data';
+
+import Header from '@/app/components/header/Header';
+import Footer from '@/app/components/footer/Footer';
+
 import styles from './page.module.css';
 
 export default function Home() {
@@ -7,11 +12,11 @@ export default function Home() {
   return (
     <div className={styles.wrap}>
       {/* header */}
+      <Header />
 
       {/* main */}
       <main className={styles.main}>
-        <h1 className={styles.title}>DUGOUT</h1>
-
+        <h1 className={styles.title}>명경기 감독 시뮬레이션 게임</h1>
         <div className={styles.grid}>
           {scenarios.map(game => (
             <Link
@@ -19,20 +24,16 @@ export default function Home() {
               href={`/games/${game.id}`}
               className={styles.gameCard}
             >
-              <div className={styles.gameDate}>
-                {game.id}
-              </div>
-
-              <div className={styles.gameTitle}>
-                {game.title}
-              </div>
+              <div className={styles.gameDate}>{game.id}</div>
+              <div className={styles.gameTitle}>{game.title}</div>
             </Link>
           ))}
         </div>
       </main>
 
       {/* footer */}
-      
+      <Footer />
+
     </div>
     
   );
