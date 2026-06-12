5f8e7e47118e9b4775bfd8e49fe08b85bbced081
Kim Jae Hyeok <master_catch@naver.com>
2026-06-13 07:05:09 +0900

    feat : 로그인 검증 추가 (임시)

---
+++ src/app/components/auth/AuthGuard.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+
+export default function AuthGuard({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+
+    if (!user && pathname !== '/login') {
+      router.replace('/login');
+    }
+  }, [pathname, router]);
+
+  return <>{children}</>;
+}
