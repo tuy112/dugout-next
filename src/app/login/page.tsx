d415de242f40d37e218a63a1761bfe50992f87a6
Kim Jae Hyeok <master_catch@naver.com>
2026-06-12 18:04:50 +0900

    feat : 로그인 페이지 추가

---
+++ src/app/login/page.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Footer from '@/app/components/footer/Footer';
+import { loginUser } from '@/data/login';
+import styles from './style.module.css';
+
+export default function LoginPage() {
+  const router = useRouter();
+
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    const user = loginUser.find(
+      item => item.id === id && item.password === password
+    );
+
+    if (!user) {
+      alert('아이디 또는 비밀번호가 올바르지 않습니다.');
+      return;
+    }
+
+    localStorage.setItem('user', JSON.stringify(user));
+
+    router.push('/');
+    router.refresh();
+  };
+
+  return (
+    <div className={styles.wrap}>
+      <main className={styles.main}>
+        <section className={styles.card}>
+            <h1 className={styles.title}>로그인</h1>
+
+            <p className={styles.desc}>
+                소셜 로그인은 추후 지원 예정입니다.
+            </p>
+
+            <div className={styles.form}>
+                <input
+                type="text"
+                placeholder="아이디"
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+                className={styles.input}
+                />
+
+            <input
+            type="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className={styles.input}
+            />
+
+            <button className={styles.submitButton} onClick={handleLogin}>로그인</button>
+            <button className={styles.loginButton} disabled>소셜 로그인 준비중</button>
+        </div>
+
+        
+        </section>
+    </main>
+
+      <Footer />
+    </div>
+  );
+}
---
+++ src/data/login.ts
@@ -0,0 +1,12 @@
+export const loginUser = [
+    {
+        id: 'admin1',
+        password: '1234',
+        name: '김재혁'
+    },
+    {
+        id: 'admin2',
+        password: '1234',
+        name: '최유상' 
+    }
+]
