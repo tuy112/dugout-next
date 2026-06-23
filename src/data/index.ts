1ed6bebd06094b72b134f135dc2ba06a4c36250d
Kim Jae Hyeok <master_catch@naver.com>
2026-06-23 10:14:32 +0900

    fix : 240901 임포트 삭제

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,13 +1,11 @@
 import { ScenarioData } from './types';
 
 import { game240817 } from './scenarios/240817';
-import { game240901 } from './scenarios/240901';
 import { game700101 } from './scenarios/700101';
 
 // 새 게임 추가 공간
 const scenarioMap: Record<string, ScenarioData> = {
   '240817': game240817,
-  '240901': game240901,
   '700101': game700101,
 };
 
